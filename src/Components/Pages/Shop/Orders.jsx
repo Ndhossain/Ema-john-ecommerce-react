2bbc28b1985726aa624814d713a441767dbf5d53
Ndhossain <hhhssnahiid@gmail.com>
2022-10-07 17:30:29 +0600

    order cart comp done

--- src/Components/Pages/Shop/Orders.jsx
+++ src/Components/Pages/Shop/Orders.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 function Orders({ cart }) {
+    const priceReduce = (type) => cart.reduce((prev, curr) => prev + curr[type], 0);
+    const tax = parseFloat(priceReduce('price') * 0.1);
+    const total = tax + priceReduce('price') + priceReduce('shipping');
+
     return (
         <section className="fixed w-[200px] bg-orange-400 h-full right-0 top-[67px] p-2">
             <h1 className="text-center text-white font-semibold text-lg px-4 py-2 rounded border border-black">
@@ -8,17 +12,20 @@ function Orders({ cart }) {
             </h1>
             <div className="mt-10">
                 <p className="flex justify-between mb-3">
-                    <span>Selected Items</span> <span> {cart.length ?? 0}</span>
+                    <span>Selected Items</span> <span> {cart.length}</span>
                 </p>
                 <p className="flex justify-between mb-3">
-                    <span>Total Price</span> <span> {cart.length ?? 0}</span>
+                    <span>Total Price</span> <span> ${priceReduce('price').toFixed(2)}</span>
                 </p>
                 <p className="flex justify-between mb-3">
-                    <span>Delivery Charge</span> <span> {cart.length ?? 0}</span>
+                    <span>Delivery Charge</span> <span> ${priceReduce('shipping').toFixed(2)}</span>
                 </p>
                 <p className="flex justify-between mb-3">
-                    <span>Tax</span> <span> {cart.length ?? 0}</span>
+                    <span>Tax</span> <span> ${tax.toFixed(2)}</span>
                 </p>
+                <h1 className="flex justify-between text-lg">
+                    <span>Grand Total</span> <span> ${total.toFixed(2)}</span>
+                </h1>
             </div>
         </section>
     );
--- src/Components/Pages/Shop/Shop.jsx
+++ src/Components/Pages/Shop/Shop.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
+import useLocalStorageForCart from '../../Hooks/useLocalStorageForCart';
 import Orders from './Orders';
 import Products from './Products';
 
 function Shop() {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([]);
-    console.log(cart);
+    const { setLocalStorage, getLocalStorage } = useLocalStorageForCart();
 
     useEffect(() => {
         fetch(
@@ -16,7 +17,25 @@ function Shop() {
             .catch((error) => console.log(error));
     }, []);
 
-    const addToCart = (product) => setCart([...cart, product]);
+    useEffect(() => {
+        const cartProduct = getLocalStorage() || {};
+        const savedCart = [];
+        Object.keys(cartProduct).forEach((keys) => {
+            const addedProduct = products.find((product) => product.id === keys);
+            if (addedProduct) {
+                console.log(addedProduct);
+                addedProduct.quantity = cartProduct[keys];
+                savedCart.push(addedProduct);
+            }
+        });
+        setCart([...savedCart]);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [products]);
+
+    const addToCart = (product) => {
+        setCart([...cart, product]);
+        setLocalStorage(product.id);
+    };
 
     return (
         <main className="">
